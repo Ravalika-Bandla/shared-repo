0632d59a2746ec08f0a2845d7301cfd912f17df2
teja384 <tejasaimudarapu@gmail.com>
2025-04-30 12:10:58 +0530

    Update mySharedStage.groovy

--- vars/mySharedStage.groovy
+++ vars/mySharedStage.groovy
@@ -1,12 +1,3 @@
-def call(String message) {
-    pipeline {
-        agent any
-        stages {
-            stage('Shared Stage') {
-                steps {
-                    echo "Running shared stage with message: ${message}"
-                }
-            }
-        }
-    }
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }
